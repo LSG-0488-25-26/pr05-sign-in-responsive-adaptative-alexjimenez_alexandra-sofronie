bd7a67f998f24fd75925419d3dd09956ebf356c4
Alesa <alexandrateodora.sofronie@gracia.lasalle.cat>
2025-12-17 20:27:12 +0100

    Estructura basica vista MediumScreen

--- PR05-Disseny responsive i adaptative/app/src/main/java/com/example/disseny_responsive_i_adaptative/ui/theme/view/MediumScreen.kt
+++ PR05-Disseny responsive i adaptative/app/src/main/java/com/example/disseny_responsive_i_adaptative/ui/theme/view/MediumScreen.kt
@@ -1,2 +1,12 @@
 package com.example.disseny_responsive_i_adaptative.ui.theme.view
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.getValue
+import androidx.navigation.NavController
+import com.example.disseny_responsive_i_adaptative.ui.theme.viewmodel.MainViewModel
+
+@Composable
+fun MediumScreen(navController: NavController, viewModel: MainViewModel) {
+
+}
