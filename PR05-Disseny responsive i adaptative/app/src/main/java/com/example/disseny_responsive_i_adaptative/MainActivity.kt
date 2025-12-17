5603d13490fceb9eb2c2635565437957591b7214
Alex Jiménez <121909423+userAl3x@users.noreply.github.com>
2025-12-17 22:43:26 +0100

    Mejoras implementadas en MainActivity

--- PR05-Disseny responsive i adaptative/app/src/main/java/com/example/disseny_responsive_i_adaptative/MainActivity.kt
+++ PR05-Disseny responsive i adaptative/app/src/main/java/com/example/disseny_responsive_i_adaptative/MainActivity.kt
@@ -6,25 +6,19 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.example.disseny_responsive_i_adaptative.ui.theme.Disseny_Responsive_i_AdaptativeTheme
-import com.example.disseny_responsive_i_adaptative.ui.theme.navigation.Routes
-import com.example.disseny_responsive_i_adaptative.ui.theme.view.CompactScreen
-import com.example.disseny_responsive_i_adaptative.ui.theme.view.ConfirmationScreen
-import com.example.disseny_responsive_i_adaptative.ui.theme.viewmodel.MainViewModel
+import com.example.disseny_responsive_i_adaptative.navigation.Routes
+import com.example.disseny_responsive_i_adaptative.view.CompactScreen
+import com.example.disseny_responsive_i_adaptative.view.ConfirmationScreen
+import com.example.disseny_responsive_i_adaptative.viewmodel.MainViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,15 +26,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Disseny_Responsive_i_AdaptativeTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize()
-                ) {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     val navController = rememberNavController()
                     val viewModel: MainViewModel = viewModel()
 
                     NavHost(
                         navController = navController,
-                        startDestination = Routes.Register.route
+                        startDestination = Routes.Register.route,
+                        modifier = Modifier.padding(innerPadding)
                     ) {
                         composable(Routes.Register.route) {
                             CompactScreen(
@@ -54,19 +47,9 @@ class MainActivity : ComponentActivity() {
                                 navArgument("username") { type = NavType.StringType }
                             )
                         ) { backStackEntry ->
-                            val arguments = backStackEntry.arguments
-                            val username: String
-                            if (arguments != null) {
-                                val tempUsername = arguments.getString("username")
-                                if (tempUsername != null) {
-                                    username = tempUsername
-                                } else {
-                                    username = ""
-                                }
-                            } else {
-                                username = ""
-                            }
-
+                            val args = backStackEntry.arguments!!   // Forzamos que los argumentos no sean nulos usando !!
+                            val username = args.getString("username", "")
+                            
                             ConfirmationScreen()
                         }
                     }
